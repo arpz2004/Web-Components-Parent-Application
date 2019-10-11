5f692455e8991f2c9602d17ee6abe4d5f7713978
Adrien Lindner <lindner.54@osu.edu>
2019-10-11 19:02:03 -0400

    Can pass object into web component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   inputValue;
+  messageIn;
+
+  ngOnInit() {
+    this.messageIn = "default";
+  }
+
+  setMessageIn(event) {
+    this.messageIn = new TestObject(event, event, event);
+  }
+}
+
+class TestObject {
+  constructor(
+    public obj1: string,
+    public obj2: string,
+    public obj3: string
+  ) { }
 }
